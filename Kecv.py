f178760879fb037b12863e34bb2ba238b0b5a757
JoseACS <js902402@gmail.com>
2018-02-19 11:28:39 -0400

    Add files via upload
    Nuevas funciones

---
+++ Kecv.py
@@ -0,0 +1,18 @@
+import sys
+from optparse import OptionParser
+
+from bot_module import Bot
+
+parser = OptionParser()
+
+parser.add_option("-c", "--conf",
+                  action="store", type="string", dest="configuration")
+(options, args) = parser.parse_args()
+
+if options.configuration is None:
+    myBot = Bot()
+else:
+    myBot = Bot(configuration=options.configuration)
+
+myBot.main()
+# El nombre del archivo es en aprecio de una gran persona que quiero mucho :*.
---
+++ config.py
@@ -0,0 +1,33 @@
+# Python IRC bot configuration file:
+
+freenode = { 
+    'network':'irc.freenode.org', 
+    'port':6667, 
+    'nick':'Hokage',
+    'channel': ['##LFKChat'],
+    'owner':'JoseACS'
+    }
+
+freenode2 = { 
+    'network':'irc.freenode.org', 
+    'port':6667, 
+    'nick':'Kecv',
+    'channel': ['##Kecv'],
+    'owner':'JoseACS'
+    }
+
+RottingIRC = {
+    'network':'irc.noteness.cf',
+    'port':6667,
+    'nick':'Hokage',
+    'channel':['#Hokage'],
+    'owner':'JoseACS'
+    }
+
+config_dict = { 
+    'freenode': freenode, 
+    'freenode2': freenode2,
+    'RomelIRC': RottingIRC
+    }
+
+
